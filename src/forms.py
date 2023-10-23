ee6347af9c0d7d013be5dedd2ffd059be84cb40e
konstantin kudryavtsev <ibelkov@gmail.com>
2023-10-23 22:27:59 +0200

    Add style for forms

--- src/forms.py
+++ src/forms.py
@@ -6,7 +6,7 @@ class SplitForm(FlaskForm):
     sub_text = TextAreaField(
         'Paste subtitles here',
         [validators.Length(max=1_000_000)],
-        render_kw={'cols': 120, 'rows': 30, 'placeholder': 'Paste subtitles here'}
+        render_kw={'cols': 120, 'rows': 10, 'placeholder': 'Paste subtitles here'}
     )
 
 
